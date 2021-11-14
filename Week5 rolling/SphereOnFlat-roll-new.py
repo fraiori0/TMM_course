d5ca7ab012c35996d17c89e8d9cb2861143f90cc
fraiori0 <fraiori@hotmail.it>
2021-11-14 10:40:24 +0100

    week5

--- Week5 rolling/SphereOnFlat-roll-new.py
+++ Week5 rolling/SphereOnFlat-roll-new.py
@@ -10,6 +10,7 @@ from sympy import sin, cos, tan, Symbol, Matrix, diag, eye, zeros
 from sympy import simplify
 import matplotlib.pyplot as plt
 import numpy as np
+from pprint import pprint
 
 """
 Montana: 'We describe the motion of obj1 relative to obj2
@@ -19,13 +20,13 @@ of Cl1(t) relative to CL2(t) at time t...''
 Montana: obj1 rolls on obj2
 """
 
-R1 = Symbol('R1', positive = True, real = True)
-u1 = Symbol('u1', real = True)
-v1 = Symbol('v1', real = True)
+R1 = Symbol('R1', positive=True, real=True)
+u1 = Symbol('u1', real=True)
+v1 = Symbol('v1', real=True)
 
-R2 = Symbol('R2', positive = True, real = True)
-u2 = Symbol('u2', real = True)
-v2 = Symbol('v2', real = True)
+R2 = Symbol('R2', positive=True, real=True)
+u2 = Symbol('u2', real=True)
+v2 = Symbol('v2', real=True)
 
 """
 What happens if obj1 is a sphere, obj 2 is flat?
@@ -36,21 +37,21 @@ Kmat1 = eye(2)
 Tmat1 = Matrix([0, tan(u2)]).transpose()
 Mmat1 = diag(1, cos(u2))
 
-#Make obj2 be a flat surface
+# Make obj2 be a flat surface
 # KTM for flat surface: no curvature, no torsion, Metric = [I]
 # One can also derive  these using fvec = Matrix([u,v,w]) and
 # following process of MontanaKmat.py
 Kmat2 = zeros(2)
-Tmat2 = zeros(1,2)
+Tmat2 = zeros(1, 2)
 Mmat2 = eye(2)
 
-#Now define symbols associated with relative velocity of obj1 on obj2
-psi = Symbol('psi', real = True)  #alignment of frames on obj1 and obj2
-omegax = Symbol('omegax', real = True)
-omegay = Symbol('omegay', real = True)
-omegaz = Symbol('omegaz', real = True)  #could be 0 if 'soft finger'
-vx = Symbol('vx', real = True) #0 if no sliding
-vy = Symbol('vy', real = True) #0 if no sliding
+# Now define symbols associated with relative velocity of obj1 on obj2
+psi = Symbol('psi', real=True)  # alignment of frames on obj1 and obj2
+omegax = Symbol('omegax', real=True)
+omegay = Symbol('omegay', real=True)
+omegaz = Symbol('omegaz', real=True)  # could be 0 if 'soft finger'
+vx = Symbol('vx', real=True)  # 0 if no sliding
+vy = Symbol('vy', real=True)  # 0 if no sliding
 #vz = 0
 
 """
@@ -69,16 +70,20 @@ du1 = simplify(Mmat1.inv() * Krel.inv() * v1gen)
 v2gen = Matrix([-omegay, omegax]) + Kmat1 * Matrix([vx, vy])
 du2 = simplify(Mmat2.inv() * Rpsi * Krel.inv() * v2gen)
 
+
 # Eq 19
 dpsi = omegaz + (Tmat1 * Mmat1 * du1 + Tmat2 * Mmat2 * du2)[0]
 
-#If there is rolling with no relative sliding then
-#vx=0, vy =0 (but note that dpsi can still be nonzero)
+# If there is rolling with no relative sliding then
+# vx=0, vy =0 (but note that dpsi can still be nonzero)
 # Eq 42
-du1 = du1.subs([(vx,0),(vy,0)])
-du2 = du2.subs([(vx,0),(vy,0)])
-dpsi = dpsi.subs([(vx,0),(vy,0)])
+du1 = du1.subs([(vx, 0), (vy, 0)])
+du2 = du2.subs([(vx, 0), (vy, 0)])
+dpsi = dpsi.subs([(vx, 0), (vy, 0)])
 
+pprint(du1)
+pprint(du2)
+pprint(dpsi)
 
 """
 Now consider what happens if we roll with some velocities about
@@ -87,62 +92,62 @@ that will represent the current value of these quantities as a
 function of time.
 """
 
-#Specify some rolling velocities about x and y
-numsteps = 100; stepsize = 1
-omegax_t = 0.05
+
+# Specify some rolling velocities about x and y
+numsteps = 100
+stepsize = 0.1
+omegax_t = 1.0
 omegay_t = 0.0
-omegaz_t = 0.0
+omegaz_t = 2.0
 
-#These variables will hold the current values of du1, du2, dpsi
-du1_t = du1.subs([(omegax,omegax_t),(omegay,omegay_t),(omegaz,omegaz_t)])
-du2_t = du2.subs([(omegax,omegax_t),(omegay,omegay_t),(omegaz,omegaz_t)])
-dpsi_t = dpsi.subs([(omegax,omegax_t),(omegay,omegay_t),(omegaz,omegaz_t)])
+# These variables will hold the current values of du1, du2, dpsi
+du1_t = du1.subs([(omegax, omegax_t), (omegay, omegay_t), (omegaz, omegaz_t)])
+du2_t = du2.subs([(omegax, omegax_t), (omegay, omegay_t), (omegaz, omegaz_t)])
+dpsi_t = dpsi.subs(
+    [(omegax, omegax_t), (omegay, omegay_t), (omegaz, omegaz_t)])
 
 
-#Suppose we start at origin on a plane and no angular misalignment: 
+# Suppose we start at origin on a plane and no angular misalignment:
 u1_t = 0
 v1_t = 0
 u2_t = 0
 v2_t = 0
 psi_t = 0
 
-#Iterate, updating du1, du2, dpsi with each step and incrementing
-#the vectors u1 and u2 on the two bodies and their relative angle, psi.
-#The points we want are [u2,v2] on obj2 (the flat surface). You can
-#look at [u1,v1] as well, but they are harder to interpret.
-plotpts = np.zeros((numsteps,3))
+# Iterate, updating du1, du2, dpsi with each step and incrementing
+# the vectors u1 and u2 on the two bodies and their relative angle, psi.
+# The points we want are [u2,v2] on obj2 (the flat surface). You can
+# look at [u1,v1] as well, but they are harder to interpret.
+plotpts = np.zeros((numsteps, 3))
 for count in range(numsteps):
-    du1_new = du1_t.subs([(u1,u1_t),(v1,v1_t),(u2,u2_t),(v2,v2_t),(psi,psi_t)])
-    du2_new = du2_t.subs([(u1,u1_t),(v1,v1_t),(u2,u2_t),(v2,v2_t),(psi,psi_t)])
-    dpsi_new = dpsi_t.subs([(u1,u1_t),(v1,v1_t),(u2,u2_t),(v2,v2_t),(psi,psi_t)])
+    du1_new = du1_t.subs(
+        [(u1, u1_t), (v1, v1_t), (u2, u2_t), (v2, v2_t), (psi, psi_t)])
+    du2_new = du2_t.subs(
+        [(u1, u1_t), (v1, v1_t), (u2, u2_t), (v2, v2_t), (psi, psi_t)])
+    dpsi_new = dpsi_t.subs(
+        [(u1, u1_t), (v1, v1_t), (u2, u2_t), (v2, v2_t), (psi, psi_t)])
     u1_t += stepsize * du1_new[0]
     v1_t += stepsize * du1_new[1]
     u2_t += stepsize * du2_new[0]
     v2_t += stepsize * du2_new[1]
     psi_t += stepsize * dpsi_new
-    plotpts[count,0] = u2_t
-    plotpts[count,1] = v2_t
-    plotpts[count,2] = psi_t
-    
+    plotpts[count, 0] = u2_t
+    plotpts[count, 1] = v2_t
+    plotpts[count, 2] = psi_t
+
 plt.figure(1)
 fig1 = plt.gcf()
-plt.axes().set_aspect('equal', 'datalim')  #square and limited by data
+plt.axes().set_aspect('equal', 'datalim')  # square and limited by data
 plt.title('trajectory u2, v2')
-plt.plot(plotpts[:,0],plotpts[:,1],color='b')
-plt.scatter(plotpts[0,0],plotpts[0,1],marker='o')
-plt.scatter(plotpts[numsteps-1,0],plotpts[numsteps-1,1],marker='X')
-            
+plt.plot(plotpts[:, 0], plotpts[:, 1], color='b')
+plt.scatter(plotpts[0, 0], plotpts[0, 1], marker='o')
+plt.scatter(plotpts[numsteps-1, 0], plotpts[numsteps-1, 1], marker='X')
+plt.show()
+
 """
 u1 and v1 are the final coordinates on the sphere; u2 and v2 are the coordinates
 on the plane
 """
-print("omegax",omegax_t,"  omegay",omegay_t,"  omegaz",omegaz_t)
-print('u1 %5.3f \t v1 %5.3f' % (u1_t,v1_t))
-print('u2 %5.3f \t v2 %5.3f' % (u2_t,v2_t))
-
-
-
-
-
-
-
+print("omegax", omegax_t, "  omegay", omegay_t, "  omegaz", omegaz_t)
+print('u1 %5.3f \t v1 %5.3f' % (u1_t, v1_t))
+print('u2 %5.3f \t v2 %5.3f' % (u2_t, v2_t))
--- Week5 rolling/Week 5.md
+++ Week5 rolling/Week 5.md
@@ -2,7 +2,7 @@
 
 Francesco Iori
 
-### Q1
+### Q 1
 
 ```python
 # We leave the derivation of the [T] and [M] matrices as an exercise
@@ -39,5 +39,49 @@ M =
 
 same as in Montana’s paper.
 
-### Q2
+### Q 2
 
+###### Q 2.1
+
+I would expect the contact point to move with $v_y = - \omega_x \cross r$ (in the plane reference frame), similarly to a cylindrical wheel. SO the point should move in the negative $y$-axis direction.
+
+The result from the script matches the expectation.
+
+###### Q 2.2
+
+Considering 
+$$
+\omega =
+\begin{pmatrix}
+	0 \\
+	\omega_y \\
+	\omega_z
+\end{pmatrix}
+\qquad
+\forall \quad
+\omega_y , \, \omega_z \in \R 
+$$
+The ball describe a circular trajectory of radius $r=\frac{\omega_y}{\omega_z}$.
+
+Example for $\omega_y = 1.0, \, \omega_z=2.0$ ($r=0.5$)
+
+
+
+![Figure_1](./Figure_1.png)
+
+
+
+###### Q 2.3
+
+It is interesting to note that we do not obtain a circle if we set $\omega = (\omega_x, 0, \omega_z )$. This can be also be seen by inspecting $\dot{u}_2 \text{ and } \dot{\psi}$, for which we have
+$$
+\begin{align}
+	\dot{\bf u}_2 &=
+		\begin{pmatrix}
+			-\omega_x \sin(\psi) - 	\omega_y \cos(\psi) \\
+			-\omega_x \cos(\psi) + \omega_y \sin(\psi)
+		\end{pmatrix}\\
+	\dot{\psi} &= \omega_x \tan(u_2) + \omega_z
+\end{align}
+$$
+To describe a circle on the plane we would like to have $\dot{\bf u}_2 $ in a form similar to $(\cos(\theta) ,\, sin(\theta))$, but the $\tan(u_2)$ term introduce an extra dependency on $u_2$. This term can be attributed to how the sphere is parametrized in the example being considered, and can be traced back to how the torsion matrix $T$ is defined for the sphere.

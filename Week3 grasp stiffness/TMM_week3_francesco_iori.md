0d11c8fed6bee203e33e3e9c01e76fb7030c04f3
fraiori0 <fraiori@hotmail.it>
2021-11-16 09:38:41 +0100

    week6 presentation material

---
+++ Week3 grasp stiffness/TMM_week3_francesco_iori.md
@@ -0,0 +1,78 @@
+# Week 3
+
+Francesco Iori
+
+### Q1
+
+Considering a jacobian of the right finger as 
+$$
+J_{\theta_2}\left[\begin{matrix}0 & - link & - link\\link & 0 & 0\\0 & - link & 0\\0 & 0 & 0\\0 & 1 & 1\\1 & 0 & 0\end{matrix}\right]
+$$
+and
+$$
+{}^P_BJ_2 = 
+\left[\begin{matrix}0 & 1 & 0 & 0 & 0 & w\\0 & 0 & 1 & 0 & - w & 0\\1 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 1 & 0\\0 & 0 & 0 & 0 & 0 & 1\\0 & 0 & 0 & 1 & 0 & 0\end{matrix}\right]
+$$
+we get
+
+```
+--- Kb2 ---
+Matrix([
+[kb + kc,  -kc,     0, 0,       0,   -kc*w],
+[    -kc,   kc,     0, 0,       0,    kc*w],
+[      0,    0,    ka, 0,   -ka*w,       0],
+[      0,    0,     0, 0,       0,       0],
+[      0,    0, -ka*w, 0, ka*w**2,       0],
+[  -kc*w, kc*w,     0, 0,       0, kc*w**2]])
+
+--- Kbtotal ---
+Matrix([
+[2*kb + 2*kc,    0,    0, 0,         0,   -2*kc*w],
+[          0, 2*kc,    0, 0,         0,         0],
+[          0,    0, 2*ka, 0,         0,         0],
+[          0,    0,    0, 0,         0,         0],
+[          0,    0,    0, 0, 2*ka*w**2,         0],
+[    -2*kc*w,    0,    0, 0,         0, 2*kc*w**2]])
+```
+
+which matches the result from the paper
+$$
+K_b = 
+\left[\begin{matrix}2 k_b + 2 k_c & 0 & 0 & 0 & 0 & - 2 k_c w\\0 & 2 k_c & 0 & 0 & 0 & 0\\0 & 0 & 2 k_a & 0 & 0 & 0\\0 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 2 k_a w^{2} & 0\\- 2 k_c w & 0 & 0 & 0 & 0 & 2 k_c w^{2}\end{matrix}\right]
+$$
+
+### Q2
+
+To consider a soft finger contat model, we modify $H$ to include torque alongthe (local) $z$-axis
+$$
+H = 
+\left[\begin{matrix}1 & 0 & 0 & 0 & 0 & 0\\0 & 1 & 0 & 0 & 0 & 0\\0 & 0 & 1 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 0 & 1\end{matrix}\right]
+$$
+Adding also the $C_{tip}$ matrix to the total compliance matrix, we obtain
+$$
+K_b = \left[\begin{matrix}4 k_a & 0 & 0 & 0 & 0 & - 2 k_a w\\0 & 2 k_a & 0 & 0 & 0 & 0\\0 & 0 & 2 k_a + 2 k_q & - 2 k_q & 0 & 0\\0 & 0 & - 2 k_q & 2 k_q & 0 & 0\\0 & 0 & 0 & 0 & 2 w^{2} \left(k_a + k_q\right) & 0\\- 2 k_a w & 0 & 0 & 0 & 0 & 2 k_a w^{2}\end{matrix}\right]
+$$
+
+### Q3
+
+$$
+K_j = 
+\left[\begin{matrix}0 & 0 & 0 & 0 & 0 & 0\\0 & - 2 f_n & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & - 2 f_n w & 0\\0 & 0 & 0 & 0 & 0 & - 2 f_n w \left(w + 1\right)\end{matrix}\right]
+$$
+
+In particular, we obtain a tilting moment, due to geometry, that would tend to unstabilize the object (but the final effect depends also on the value of $K_b$).
+
+### Q4
+
+By adding the translation (in the local $\{l_i,m_i,n_i\}$ reference frame) produced by the infinitesimal rotation in `diff_i`(considered also to be in the local reference frame), we obtain a new $\Delta J$ .
+
+This result in a new geometric stiffness
+$$
+K_j = 
+\left[\begin{matrix}0 & 0 & 0 & 0 & 0 & 0\\0 & - 2 f_n & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 2 f_n \left(R - w\right) & 0\\0 & 0 & 0 & 0 & 0 & 2 f_n \left(R - w\right) \left(w + 1\right)\end{matrix}\right]
+$$
+If we consider the torque produced by this geometric effect around the body's $z$-axis,
+$$
+\tau_{b,z}^j = 2 f_n \left(R - w\right) \left(w + 1\right) \, \delta \theta_z^2
+$$
+we can see that it is positive (i.e. restoring torque) for $R>w$. So, for an object that is comparatively thin compared to the radius of the fingertips, the geometric effect is also stabilizing.
--- Week3 grasp stiffness/Week3_homework_gojko/Q3+Q4-(Kj_left-finger-modfied).py
+++ Week3 grasp stiffness/Week3_homework_gojko/Q3+Q4-(Kj_left-finger-modfied).py
@@ -28,8 +28,7 @@ Select and evaluate a few lines at a time, checking intermediate results as
 you go to make sure they make sense.
 """
 
-from sympy import sin, cos, pi, Matrix, Symbol, symbols, simplify, pprint, latex
-from sys import exit
+from sympy import sin, cos, pi, Matrix, Symbol, symbols, simplify, pprint
 
 #################################
 # Define a 6 element wrench and 6 element body twist
@@ -87,14 +86,8 @@ Hmat = (Matrix.eye(3)).row_join(Matrix.zeros(3))  # 3x6
 link = Symbol('link', real=True, positive=True)  # link length
 Jq1 = Matrix([[0, -link, -link], [link, 0, 0], [0, link, 0],
              [0, 0, 0], [0, 1, 1], [-1, 0, 0]])
-Jq2 = Matrix([
-    [0, -link, -link],
-    [link, 0, 0],
-    [0, -link, 0],
-    [0, 0, 0],
-    [0, 1, 1],
-    [1, 0, 0]
-])
+Jq2 = Matrix([[0, -link, -link], [link, 0, 0],
+             [0, -link, 0], [0, 0, 0], [0, 1, 1], [1, 0, 0]])
 
 # Helps to have a little coord. frame toy when building these
 
@@ -103,9 +96,6 @@ dq1, dq2, dq3 = symbols('dq1,dq2,dq3', real=True)  # small joint motions
 djoints = Matrix([dq1, dq2, dq3])
 dfcontact = Jq1*djoints    # total twist motion of the fingertip
 dtrans = Hmat*Jq1*djoints  # 3 element vector of velocities in (l,m,n) frame
-
-dfcontact2 = Jq2*djoints    # total twist motion of the fingertip
-dtrans2 = Hmat*Jq2*djoints  # 3 element vector of velocities in (l,m,n) frame
 
 
 #########################################################
@@ -117,7 +107,7 @@ w = Symbol('w', real=True, positive=True)
 Jb1t = Jbtran.subs([(rx, -w), (ry, 0), (rz, 0), (thetax, 0),
                    (thetay, -pi/2), (thetaz, -pi/2)])
 Jb2t = Jbtran.subs([(rx, w), (ry, 0), (rz, 0), (thetax, 0),
-                    (thetay, pi/2), (thetaz, pi/2)])
+                   (thetay, pi/2), (thetaz, pi/2)])
 
 """
 Up to this point everything has been the same as for getting [Kb] the "direct"
@@ -152,19 +142,19 @@ Make a small motion of the body and find the corresponding
 motions of the fingers
 """
 bctwist1 = Jb1t.T*bodytwist  # contact frames on the body
-bctwist2 = Jb2t.T*bodytwist  # contact frames on the body
+bctwist2 = Jb2t.T*bodytwist
 
 ctwist1 = Hmat*bctwist1  # what gets transmitted through the contact
-ctwist2 = Hmat*bctwist2  # what gets transmitted through the contact
+ctwist2 = Hmat*bctwist2
 
 Jq1inv = (Hmat*Jq1)**-1
 Jq2inv = (Hmat*Jq2)**-1
 
 djoints1 = Jq1inv * ctwist1  # corresponding joint motions
-djoints2 = Jq2inv * ctwist2  # corresponding joint motions
+djoints2 = Jq2inv * ctwist2
 
 ftwist1 = Jq1*djoints1  # Work back outward to get the total motion
-ftwist2 = Jq2*djoints2  # Work back outward to get the total motion
+ftwist2 = Jq2*djoints2
 
 # Simplify a bit by letting 'link' = 1 as in example
 ftwist1 = ftwist1.subs(link, 1)
@@ -173,19 +163,19 @@ ftwist2 = ftwist2.subs(link, 1)
 # Relative motion of fingertip to object in (l,m,n) frame
 diff1 = ftwist1-bctwist1
 diff2 = ftwist2-bctwist2
-pprint(diff1)
 
 # Put the elements of diff1 and diff2 into our differential transforms:
-dtheta = Symbol('dtheta', real=True)
-R = Symbol('R', real=True)
 dJtran1p = deltaJtran.subs([(dqx, diff1[3]), (dqy, diff1[4]),
-                           (dqz, diff1[5]), (deltax, R*diff1[4]), (deltay, -R*diff1[3]), (deltaz, 0)])
+                           (dqz, diff1[5]), (deltax, 0), (deltay, 0), (deltaz, 0)])
 dJtran2p = deltaJtran.subs([(dqx, diff2[3]), (dqy, diff2[4]),
-                           (dqz, diff2[5]), (deltax, R*diff2[4]), (deltay, -R*diff2[3]), (deltaz, 0)])
-# dJtran1p = deltaJtran.subs([(dqx, diff1[3]), (dqy, diff1[4]),
-#                            (dqz, diff1[5]), (deltax, 0), (deltay, 0), (deltaz, 0)])
-# dJtran2p = deltaJtran.subs([(dqx, diff2[3]), (dqy, diff2[4]),
-#                            (dqz, diff2[5]), (deltax, 0), (deltay, 0), (deltaz, 0)])
+                           (dqz, diff2[5]), (deltax, 0), (deltay, 0), (deltaz, 0)])
+
+"""Modifications for Q4"""
+r = Symbol('r', real=True, positive=True)
+dJtran1pQ4 = deltaJtran.subs([(dqx, diff1[3]), (dqy, diff1[4]), (
+    dqz, diff1[5]), (deltax, r*diff1[4]), (deltay, -r*diff1[3]), (deltaz, 0)])
+dJtran2pQ4 = deltaJtran.subs([(dqx, diff2[3]), (dqy, diff2[4]), (
+    dqz, diff2[5]), (deltax, r*diff2[4]), (deltay, -r*diff2[3]), (deltaz, 0)])
 # Note: This is where we would change things for rolling (e.g., 'deltax' not zero)
 # Note also that there could be rolling in two orthogonal directions.
 
@@ -196,21 +186,25 @@ fgrasp = Matrix([0, 0, -fn, 0, 0, 0])
 dfbody1 = -Jb1t*dJtran1p*fgrasp
 dfbody2 = -Jb2t*dJtran2p*fgrasp
 
-print("Change in force on body due to small geometry change [dfbody]")
-pprint(dfbody1)
-pprint(dfbody2)
+dfbody1Q4 = -Jb1t*dJtran1pQ4*fgrasp
+dfbody2Q4 = -Jb2t*dJtran2pQ4*fgrasp
 
+#print("Change in force on body due to small geometry change [dfbody]")
+# pprint(dfbody1)
 
 """
 You will add the corresponding terms for the right finger above and then
 get the total change in the body force. Then you can use sympy to take
 the jacobian of dfbody with respect to bodytwist to get [Kj]
 """
-print("\n--- dfbody_tot ---")
 dfbody = simplify(dfbody1+dfbody2)
-pprint(dfbody)
-
-print("\n--- Kj ---")
-Kj = simplify(dfbody.jacobian(bodytwist))  # Matches Kj in eq (30) Cutkosky&Kao
+Kj = dfbody.jacobian(bodytwist)  # Matches Kj in eq (30) Cutkosky&Kao
+print("Geometric stifness:")
 pprint(Kj)
-print(latex(Kj))
+
+dfbodyQ4 = simplify(dfbody1Q4+dfbody2Q4)
+KjQ4 = dfbodyQ4.jacobian(bodytwist)
+print("Geometric stifness for hemispherical fingertip:")
+pprint(KjQ4)
+
+print("Critical value r=w, Kj >= 0")
